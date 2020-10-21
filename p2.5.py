f7843e4393f7fc41b14c0f000dddca8eaf56c644
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-10-21 23:10:42 +0700

    Create p2.5.py

---
+++ p2.5.py
@@ -0,0 +1,13 @@
+#9
+#kotak bintang ajaib
+kolom = 5
+baris = 5
+
+i = 0
+while (i < baris):
+    j = 0
+    while (j < kolom):
+        print('*', end='')
+        j += 1
+    print('')
+    i += 1
